014900eba54ef4c62707591d9cfeab98a355eb2f
Laureano <laau260@gmail.com>
2022-02-20 15:05:48 -0300

    Se añadieron funcionalidades como: crear y conectar elemento al dom, elegir palabra al azar

---
+++ ahorcado.js
@@ -0,0 +1,59 @@
+var lista = ["Alura", "Oracle", "Computadora"];
+
+function aleatorio(n){
+
+    return (Math.floor((Math.random()*n)))
+
+}
+
+console.log(aleatorio(lista.length));
+
+function elegirPalabra(lista) {
+
+    return lista[aleatorio(lista.length)];
+
+}
+
+function cambiarAGuiones(palabra){
+
+    var nuevo = palabra.split('');
+    console.log(nuevo)
+    return nuevo;
+}
+
+function crearElementoGuion(){
+
+    //CREAMOS <P>
+    const nuevoP = document.createElement("p");
+    const contenido = document.createTextNode("_");
+    nuevoP.appendChild(contenido);
+    nuevoP.className = "palabra-actual";
+    //CONECTAMOS P
+    const pActual = document.querySelector("#palabra-ahorcado");
+    pActual.insertAdjacentElement("afterbegin", nuevoP);
+    console.log("Elemento creado: ", nuevoP);
+}
+
+function crearPalabraOculta (palabra) {
+
+    for (i=0; i<palabra.length; i++){
+
+        crearElementoGuion();
+    }
+
+}
+
+
+console.log(elegirPalabra(lista));
+
+var botonSiguiente = document.querySelector("#boton-siguiente");
+botonSiguiente.addEventListener("click", function(){
+
+    botonSiguiente.innerHTML = "Siguiente";
+    var palabraActual = elegirPalabra(lista);
+    crearPalabraOculta(palabraActual);
+    console.log(palabraActual);
+  
+    
+});
+
